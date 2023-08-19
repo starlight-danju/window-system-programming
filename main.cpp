8a9fe95425788faa3488b5034d20d8727410a600
StarlightSangdan <danju@starlight-sangdan.com>
2023-08-20 08:08:45 +0900

    sample: Add create Thread sample code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <Windows.h>
+
+DWORD WINAPI ThreadFunction(LPVOID pParam)
+{
+	std::cout << "TreadFunction() - Begin" << std::endl;
+	std::cout << (const char*)pParam << std::endl;
+	std::cout << "ThreadFunction() - End" << std::endl;
+	return 0;
+}
 
 int main()
 {
-	// initial commit
+	std::cout << "main() - Begin" << std::endl;
+	DWORD dwThreadId = 0;
+	HANDLE hThread = ::CreateThread(
+		NULL,
+		0,
+		ThreadFunction,
+		(LPVOID)"PARAM",
+		0,
+		&dwThreadId);
+
+	if (hThread == NULL) {
+
+	}
+
+	::WaitForSingleObject(hThread, INFINITE);
+	::CloseHandle(hThread);
+
+	std::cout << "main() - End" << std::endl;
 	return 0;
 }
